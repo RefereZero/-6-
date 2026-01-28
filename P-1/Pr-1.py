1b530d8c3424f484dadf3a2eadcf83e191bcd706
RefereZero <referezero@gmail.com>
2026-01-28 14:48:32 +0300

    Add files via upload

---
+++ P-1/Pr-1.py
@@ -0,0 +1,7 @@
+name_surname = input("Ваши фамилия, имя? ")
+age = input("Сколько Вам лет? ")
+location = input("Где вы живете? ")
+
+print("\nВаши фамилия, имя:", name_surname)
+print("Ваш возраст:", age)
+print("Вы живете в", location)
