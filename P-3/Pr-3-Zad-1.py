cdb1b5eb5455875326d22c7318a21aab7b1371ae
RefereZero <referezero@gmail.com>
2026-01-28 14:48:12 +0300

    Delete P-3 directory

--- P-3/Pr-3-Zad-1.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-if num1 > num2:
-    print(f"Наибольшее число: {num1}")
-elif num2 > num1:
-    print(f"Наибольшее число: {num2}")
-else:
-    print("Числа равны")
--- P-3/Pr-3-Zad-2.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Введите целое число: "))
-if num % 2 == 0:
-    print(f"Число {num} четное")
-else:
-    print(f"Число {num} нечетное")
