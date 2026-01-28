f973238dfdd1129335ca2971252dc35fc0d260bb
RefereZero <referezero@gmail.com>
2026-01-28 14:48:02 +0300

    Delete Pr-1.py

--- Pr-1.py
+++
@@ -1,7 +0,0 @@
-name_surname = input("Ваши фамилия, имя? ")
-age = input("Сколько Вам лет? ")
-location = input("Где вы живете? ")
-
-print("\nВаши фамилия, имя:", name_surname)
-print("Ваш возраст:", age)
-print("Вы живете в", location)
